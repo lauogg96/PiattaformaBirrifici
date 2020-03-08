02ce98d1fb0b3e1ada0a19070f74428248ea538e
atlandi95 <32636176+atlandi95@users.noreply.github.com>
2020-03-08 12:21:03 +0100

    Update TC75Test.java

--- Documentazione/Testing di sistema/tc7/TC75Test.java
+++ Documentazione/Testing di sistema/tc7/TC75Test.java
@@ -38,7 +38,7 @@ public class TC75Test {
   }
   @Test
   public void tC75() {
-    driver.get("http://localhost:8080/Piattaforma_Birrifici/Home.jsp");
+    driver.get("http://localhost:8081/Piattaforma_Birrifici/Home.jsp");
     driver.manage().window().setSize(new Dimension(1382, 744));
     driver.findElement(By.cssSelector("b > #cerca")).click();
     driver.findElement(By.cssSelector(".butCerca")).click();
