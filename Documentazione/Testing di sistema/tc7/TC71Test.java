296bc829dc0d68cfa5eb395bf76fc0084d5dbcd8
atlandi95 <32636176+atlandi95@users.noreply.github.com>
2020-03-08 12:21:09 +0100

    Update TC71Test.java

--- Documentazione/Testing di sistema/tc7/TC71Test.java
+++ Documentazione/Testing di sistema/tc7/TC71Test.java
@@ -38,7 +38,7 @@ public class TC71Test {
   }
   @Test
   public void tC71() {
-    driver.get("http://localhost:8080/Piattaforma_Birrifici/Home.jsp/Piattaforma_Birrifici/Home.jsp");
+    driver.get("http://localhost:8081/Piattaforma_Birrifici/Home.jsp/Piattaforma_Birrifici/Home.jsp");
     driver.manage().window().setSize(new Dimension(1382, 744));
     driver.findElement(By.cssSelector(".butCerca")).click();
   }
