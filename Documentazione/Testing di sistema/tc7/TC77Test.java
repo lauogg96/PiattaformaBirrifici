ca8c906c4ddddc1385050e659d4cbc46dec3e7c9
atlandi95 <32636176+atlandi95@users.noreply.github.com>
2020-03-08 12:21:00 +0100

    Update TC77Test.java

--- Documentazione/Testing di sistema/tc7/TC77Test.java
+++ Documentazione/Testing di sistema/tc7/TC77Test.java
@@ -38,7 +38,7 @@ public class TC77Test {
   }
   @Test
   public void tC77() {
-    driver.get("http://localhost:8080/Piattaforma_Birrifici/Home.jsp");
+    driver.get("http://localhost:8081/Piattaforma_Birrifici/Home.jsp");
     driver.manage().window().setSize(new Dimension(1382, 744));
     driver.findElement(By.cssSelector("b > #cerca")).click();
     driver.findElement(By.cssSelector("b > #cerca")).sendKeys("Bionda+-;");
