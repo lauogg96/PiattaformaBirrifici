970aacef4ab7f9540c59a780754f3755e41b1533
Laura Oggiano <l.oggiano@studenti.unisa.it>
2020-03-08 12:18:22 +0100

    Update TC57Test.java

--- Documentazione/Testing di sistema/tc5/TC57Test.java
+++ Documentazione/Testing di sistema/tc5/TC57Test.java
@@ -38,7 +38,7 @@ public class TC57Test {
   }
   @Test
   public void tC57() {
-    driver.get("http://localhost:8080/Piattaforma_Birrifici/Home.jsp");
+    driver.get("http://localhost:8081/Piattaforma_Birrifici/Home.jsp");
     driver.manage().window().setSize(new Dimension(1382, 744));
     driver.findElement(By.linkText("Login Venditore")).click();
     driver.findElement(By.cssSelector(".but")).click();
