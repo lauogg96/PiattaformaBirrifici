225ac0e5983496eb0458a768ac9def6a23628948
atlandi95 <32636176+atlandi95@users.noreply.github.com>
2020-03-08 12:16:25 +0100

    Update TC26Test.java

--- Documentazione/Testing di sistema/tc2/TC26Test.java
+++ Documentazione/Testing di sistema/tc2/TC26Test.java
@@ -38,7 +38,7 @@ public class TC26Test {
   }
   @Test
   public void tC26() {
-    driver.get("http://localhost:8080/Piattaforma_Birrifici/Home.jsp");
+    driver.get("http://localhost:8081/Piattaforma_Birrifici/Home.jsp");
     driver.manage().window().setSize(new Dimension(1382, 744));
     driver.findElement(By.cssSelector(".slidesotto:nth-child(1) li:nth-child(2) > a")).click();
     driver.findElement(By.id("in")).click();
