56ed9bf9704d31df03d7a8f5242b181765348c1d
atlandi95 <32636176+atlandi95@users.noreply.github.com>
2020-03-30 17:47:57 +0200

    Update Cerca.java

--- Codice/Piattaforma_Birrifici/src/Control/Cerca.java
+++ Codice/Piattaforma_Birrifici/src/Control/Cerca.java
@@ -17,33 +17,42 @@ import javax.servlet.http.HttpSession;
 public class Cerca extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Cerca() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession htp=request.getSession(); 
-		String email=(String)htp.getAttribute("email");
-		htp.setAttribute("email",email);
-		String cerca=request.getParameter("cerca");
-		request.setAttribute("cerca",cerca);
-		RequestDispatcher view =  request.getRequestDispatcher("ListaProdView.jsp");
-		view.forward(request, response);
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession htp=request.getSession(); 
+		String email=(String)htp.getAttribute("email");
+		htp.setAttribute("email",email);
+		String cerca=request.getParameter("cerca");
+		
+		int cont=0;
+		String result="false";
+	try {	
+		if((!cerca.matches("^[a-zA-Z ]*$")||cerca.length()>24)&&(!cerca.matches("^[0-9]{11}$"))&&(!cerca.matches("^[a-zA-Z ]*$")||cerca.length()>24)){
+			request.setAttribute("failCerca", "cerca");
+			response.getWriter().write(result);
+			RequestDispatcher view = request.getRequestDispatcher("errore.jsp");
+			view.forward(request, response);
+			cont++;
+		}
+		
+		if(cont==0) {
+			request.setAttribute("cerca",cerca);
+			result="true";
+			response.getWriter().write(result);
+			RequestDispatcher view =  request.getRequestDispatcher("ListaProdView.jsp");
+			view.forward(request, response);
+		}
+	}catch(Exception e){
+		e.printStackTrace();
+		response.getWriter().write(result);
+	}
+	
+	}
+
+
+	
 }
